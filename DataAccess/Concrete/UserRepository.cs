28f19f9ecce0249b202e53a7f9194eeaf262c168
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-10 17:44:54 +0300

    Concrete of User Repository created

---
+++ DataAccess/Concrete/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using Core.DataAccess.Concrete;
+using Core.Entities.Concretes;
+using DataAccess.Abstract;
+using DataAccess.Concrete.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class UserRepository : EntityRepositoryBase<User, BaseDbContext>, IUserRepository
+    {
+        public UserRepository(BaseDbContext context) : base(context)
+        {
+        }
+    }
+}
