e22ee4b38f0dda0243708f9202770d45235a39ca
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-10 17:44:34 +0300

    DbContext created

---
+++ DataAccess/Concrete/Context/BaseDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities.Concretes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.Context
+{
+    public class BaseDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EczanemDb;Trusted_Connection=true");
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
