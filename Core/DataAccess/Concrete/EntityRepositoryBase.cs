f31e16193b1637079f5a1cd217799a2708789236
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-10 17:37:16 +0300

    Concrete of Entity Repository Base created

---
+++ Core/DataAccess/Concrete/EntityRepositoryBase.cs
@@ -0,0 +1,54 @@
+﻿using Core.DataAccess.Abstract;
+using Core.Entities.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.Concrete
+{
+    public class EntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+
+        where TEntity : class, IEntity
+        where TContext : DbContext
+    {
+        public EntityRepositoryBase(TContext context)
+        {
+            Context = context;
+        }
+
+        protected TContext Context { get; }
+
+        public TEntity Add(TEntity entity)
+        {
+            return Context.Add(entity).Entity;
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            Context.Update(entity);
+            return entity;
+        }
+
+        public void Delete(TEntity entity)
+        {
+            Context.Remove(entity);
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            return filter == null
+                ? Context.Set<TEntity>().ToList()
+                : Context.Set<TEntity>().Where(filter).ToList();
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            return Context.Set<TEntity>().FirstOrDefault(filter);
+        }
+    }
+}
+
