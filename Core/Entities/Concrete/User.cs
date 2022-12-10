3fc0d7f3cef4e9df26e9d8a177f0b82563e082ed
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-10 17:43:50 +0300

    Entity Properties updated

--- Core/Entities/Concrete/User.cs
+++ Core/Entities/Concrete/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,17 +7,19 @@ using System.Threading.Tasks;
 
 namespace Core.Entities.Concretes
 {
-    public class User : Entity
+    public class User : IEntity
     {
+        public int Id { get; set; }
+        public long TurkishIdentityNumber { get; set; }
         public string FirstName { get; set; }
         public string? MiddleName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-
+        public DateTime BirthDate { get; set; }
+        public string MobilePhone { get; set; }
+        public string? Address { get; set; }
         public byte[] PasswordSalt { get; set; }
-
         public byte[] PasswordHash { get; set; }
-
         public bool Status { get; set; }
     }
 }
