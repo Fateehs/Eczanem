3dbf2adceb909e9e79a2f5bf7ac7612c17c27728
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-11 18:40:55 +0300

    Services added for Dependency Injection

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,6 +1,17 @@
+using Business;
+using Core;
+using DataAccess;
+using DataAccess.Concrete.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddBusinessServices();
+builder.Services.AddCoreServices();
+builder.Services.AddDataAccessServices();
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddDbContext<BaseDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
