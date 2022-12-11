afa1acff366fc269076d3e2a6dbae3ef1e2d142d
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-11 14:30:28 +0300

    Concrete of Business Service created

--- Business/Concrete/UserManager.cs
+++ Business/Concrete/UserManager.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Business.Abstract;
+using Core.Entities.Concretes;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,44 @@ using System.Threading.Tasks;
 
 namespace Business.Concrete
 {
-    public class UserManager
+    public class UserManager : IUserService
     {
+        IUserRepository _userRepository;
+
+        public UserManager(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public IDataResult<List<User>> GetAll()
+        {
+            return new SuccessDataResult<List<User>>(_userRepository.GetAll());
+        }
+
+        public IDataResult<User> GetById(int id)
+        {
+            return new SuccessDataResult<User>(_userRepository.Get(u => u.Id == id));
+        }
+
+        public IResult Add(User user)
+        {
+            _userRepository.Add(user);
+
+            return new SuccessResult();
+        }
+
+        public IResult Update(User user)
+        {
+            _userRepository.Update(user);
+
+            return new SuccessResult();
+        }
+
+        public IResult Delete(User user)
+        {
+            _userRepository.Delete(user);
+
+            return new SuccessResult();
+        }
     }
 }
