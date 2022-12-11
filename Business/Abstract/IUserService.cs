c7daadbe4a43dde7d7ca116e93ec4e95bd25c11c
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-11 14:30:18 +0300

    Abstract of Business Service created

--- Business/Abstract/IUserService.cs
+++ Business/Abstract/IUserService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Core.Entities.Concretes;
+using Core.Utilities.Results.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,10 @@ namespace Business.Abstract
 {
     public interface IUserService
     {
+        IDataResult<List<User>> GetAll();
+        IDataResult<User> GetById(int id);
+        IResult Add(User user);
+        IResult Update(User user);
+        IResult Delete(User user);
     }
 }
