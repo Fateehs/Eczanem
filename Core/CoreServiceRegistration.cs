67f4bec851bf8c755d01b9af7e002d10a4402326
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-11 18:38:36 +0300

    Scopes added for Dependency Injection

---
+++ Core/CoreServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Core.DataAccess.Abstract;
+using Core.DataAccess.Concrete;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public static class CoreServiceRegistration
+    {
+        public static IServiceCollection AddCoreServices(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
---
+++ DataAccess/DataAccessServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using DataAccess.Abstract;
+using DataAccess.Concrete;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public static class DataAccessServiceRegistration
+    {
+        public static IServiceCollection AddDataAccessServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            return services;
+        }
+    }
+}
