d8bf38ca28463213a8fb70d635b62fb9c46b3184
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-13 18:38:31 +0300

    Controller created, Methods added

---
+++ WebAPI/Controllers/AuthsController.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstract;
+using Core.Entities.Concrete.DTOs;
+using Core.Utilities.Results.Concrete;
+using Core.Utilities.Security.JWT;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthsController : ControllerBase
+    {
+        private IAuthService _authService;
+
+        public AuthsController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(RegisterDTO registerDTO)
+        {
+            var result = _authService.Register(registerDTO, registerDTO.Password);
+            if (!result.Success) return BadRequest(result);
+
+            var createAccessTokenResult = _authService.CreateAccessToken(result.Data);
+            if (!result.Success) return BadRequest(result);
+
+            var createdAccessTokenDataWithSuccessResult = new SuccessDataResult<AccessToken>(createAccessTokenResult.Data);
+
+            return Ok(createdAccessTokenDataWithSuccessResult);
+        }
+    }
+}
