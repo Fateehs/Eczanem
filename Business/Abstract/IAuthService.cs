a0c68f648bdfee38f737cbacc4aed53af2df8d6d
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-13 18:31:29 +0300

    Abstract Service created

---
+++ Business/Abstract/IAuthService.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities.Concrete.DTOs;
+using Core.Entities.Concretes;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Security.JWT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IAuthService
+    {
+        IDataResult<User> Register(RegisterDTO registerDTO, string password);
+        IDataResult<AccessToken> CreateAccessToken(User user);
+    }
+}
