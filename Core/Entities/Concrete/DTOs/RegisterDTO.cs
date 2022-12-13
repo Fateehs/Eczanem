a729c68e464f3237088d5393a2e5708e96ab6cf1
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-13 18:35:10 +0300

    DTO created, Properties added

---
+++ Core/Entities/Concrete/DTOs/RegisterDTO.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.Concrete.DTOs
+{
+    public class RegisterDTO : IDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
