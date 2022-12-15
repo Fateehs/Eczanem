b68ca11db6b67adb6adb21da814b0a6a6eb97f40
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-15 14:14:42 +0300

    Usings modified

--- Core/CoreServiceRegistration.cs
+++ Core/CoreServiceRegistration.cs
@@ -1,6 +1,4 @@
-﻿using Core.DataAccess.Abstract;
-using Core.DataAccess.Concrete;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
