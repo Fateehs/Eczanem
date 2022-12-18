087d1e811c3703403564615cad93515324cb78a6
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-18 17:12:48 +0300

    Concrete of Data Access created, Constructor added

---
+++ DataAccess/Concrete/CustomerRepository.cs
@@ -0,0 +1,24 @@
+﻿using Core.DataAccess.Concrete;
+using Core.Entities.Concrete.DTOs;
+using Core.Entities.Concretes;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.Context;
+using Entities.Concrete;
+using Entities.Concrete.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class CustomerRepository : EntityRepositoryBase<Customer, BaseDbContext>, ICustomerRepository
+    {
+        public CustomerRepository(BaseDbContext context) : base(context)
+        {
+        }
+    }
+}
