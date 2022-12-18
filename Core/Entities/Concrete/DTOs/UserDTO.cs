fe649a481b6bf443cf36025096e8585a19aa62d2
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-18 17:11:46 +0300

    DTO Created, Properties added

---
+++ Core/Entities/Concrete/DTOs/UserDTO.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.Concrete.DTOs
+{
+    public class UserDTO : IDTO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
