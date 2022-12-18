95ebf4aec0ff04e4b55ffe080afcacb6479ffc7d
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-18 17:12:30 +0300

    Abstract of Data Access created

---
+++ DataAccess/Abstract/ICustomerRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core.DataAccess.Abstract;
+using Core.Entities.Concrete.DTOs;
+using Core.Entities.Concretes;
+using Entities.Concrete;
+using Entities.Concrete.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface ICustomerRepository : IEntityRepository<Customer>
+    {
+    }
+}
