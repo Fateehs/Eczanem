41270beeb1a9c22b81e50b5f421e0015c3ca2786
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-19 17:13:50 +0300

    Controller created, Select and CRUD Operations added

---
+++ WebAPI/Controllers/CouriersController.cs
@@ -0,0 +1,64 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouriersController : ControllerBase
+    {
+        ICourierService _courierService;
+
+        public CouriersController(ICourierService courierService)
+        {
+            _courierService = courierService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var result = _courierService.GetAll();
+
+            if (!result.Success) return BadRequest(result);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public IActionResult GetById(int id)
+        {
+            var result = _courierService.GetById(id);
+
+            if (!result.Success) return BadRequest(result);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Courier courier)
+        {
+            var result = _courierService.Add(courier);
+
+            if (!result.Success) return BadRequest(result);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Courier courier)
+        {
+            var result = _courierService.Update(courier);
+
+            if (!result.Success) return BadRequest(result);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var result = _courierService.Delete(id);
+
+            if (!result.Success) return BadRequest(result);
+            return Ok(result);
+        }
+    }
+}
---
+++ WebAPI/Controllers/PharmaciesController.cs
@@ -0,0 +1,65 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results.Concrete;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PharmaciesController : ControllerBase
+    {
+        IPharmacyService _pharmacyService;
+
+        public PharmaciesController(IPharmacyService pharmacyService)
+        {
+            _pharmacyService = pharmacyService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _pharmacyService.GetAll();
+
+            if (!result.Success) return BadRequest(result);
+            return Ok(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _pharmacyService.GetById(id);
+
+            if (!result.Success) return BadRequest(result);
+            return Ok(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Pharmacy pharmacy)
+        {
+            var result = _pharmacyService.Add(pharmacy);
+
+            if (!result.Success) return BadRequest(result);
+            return Ok(result);
+        }
+
+        [HttpPost("update")]
+        public IActionResult Update(Pharmacy pharmacy)
+        {
+            var result = _pharmacyService.Update(pharmacy);
+
+            if (!result.Success) return BadRequest(result);
+            return Ok(result);
+        }
+
+        [HttpPost("delete")]
+        public IActionResult Delete(int id)
+        {
+            var result = _pharmacyService.Delete(id);
+
+            if (!result.Success) return BadRequest(result);
+            return Ok(result);
+        }
+    }
+}
