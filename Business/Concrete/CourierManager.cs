423fbaf2171007a5edb336f525590963e1689cef
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-19 17:09:57 +0300

    Manager created, CRUD and Select Operations added

---
+++ Business/Concrete/CourierManager.cs
@@ -0,0 +1,57 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CourierManager : ICourierService
+    {
+        ICourierRepository _courierRepository;
+
+        public CourierManager(ICourierRepository courierRepository)
+        {
+            _courierRepository = courierRepository;
+        }
+
+        public IDataResult<List<Courier>> GetAll()
+        {
+            return new SuccessDataResult<List<Courier>>(_courierRepository.GetAll());
+        }
+
+        public IDataResult<Courier> GetById(int id)
+        {
+            return new SuccessDataResult<Courier>(_courierRepository.Get(c => c.Id == id));
+        }
+
+        public IResult Add(Courier courier)
+        {
+            _courierRepository.Add(courier);
+
+            return new SuccessResult();
+        }
+
+        public IResult Update(Courier courier)
+        {
+            _courierRepository.Update(courier);
+
+            return new SuccessResult();
+        }
+
+        public IResult Delete(int id)
+        {
+            var courier = _courierRepository.Get(c => c.Id == id);
+
+            _courierRepository.Delete(courier);
+
+            return new SuccessResult();
+        }
+    }
+}
---
+++ Business/Concrete/PharmacyManager.cs
@@ -0,0 +1,58 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PharmacyManager : IPharmacyService
+    {
+        IPharmacyRepository _pharmacyRepository;
+
+        public PharmacyManager(IPharmacyRepository pharmacyRepository)
+        {
+            _pharmacyRepository = pharmacyRepository;
+        }
+
+        public IDataResult<List<Pharmacy>> GetAll()
+        {
+            return new SuccessDataResult<List<Pharmacy>>(_pharmacyRepository.GetAll());
+        }
+
+        public IDataResult<Pharmacy> GetById(int id)
+        {
+            return new SuccessDataResult<Pharmacy>(_pharmacyRepository.Get(p => p.Id == id));
+        }
+
+        public IResult Add(Pharmacy pharmacy)
+        {
+            _pharmacyRepository.Add(pharmacy);
+
+            return new SuccessResult();
+        }
+
+        public IResult Update(Pharmacy pharmacy)
+        {
+            _pharmacyRepository.Update(pharmacy);
+
+            return new SuccessResult();
+        }
+
+        public IResult Delete(int id)
+        {
+            var pharmacy = _pharmacyRepository.Get(p => p.Id == id);
+
+            if (pharmacy != null)
+
+                _pharmacyRepository.Delete(pharmacy);
+
+            return new SuccessResult();
+        }
+    }
+}
