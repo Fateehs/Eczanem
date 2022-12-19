888626a9751622ef3975da54645b8503295be32e
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-19 17:12:58 +0300

    DTO Created, Properties added

--- Entities/Concrete/DTOs/CourierDTO.cs
+++ Entities/Concrete/DTOs/CourierDTO.cs
@@ -7,18 +7,24 @@ using System.Threading.Tasks;
 
 namespace Entities.Concrete.DTOs
 {
-    public class CustomerDTO : IDTO
+    public class CourierDTO : IDTO
     {
         public int Id { get; set; }
         public int UserId { get; set; }
+        public string IdentityNumber { get; set; }
         public string? FirstName { get; set; }
         public string? MiddleName { get; set; }
         public string? LastName { get; set; }
         public string? Email { get; set; }
-        public string IdentityNumber { get; set; }
-        public DateTime BirthDay { get; set; }
         public string PhoneNumber { get; set; }
-        public string Address { get; set; }
         public string Location { get; set; }
+        public string Transport { get; set; }
+        public string PlateNumber { get; set; }
+        public bool OnDuty { get; set; }
+        public bool OffDuty { get; set; }
+        public DateTime OnDutyTime { get; set; }
+        public DateTime OffDutyTime { get; set; }
+        public string ServiceStatus { get; set; }
+        public string Status { get; set; }
     }
 }
