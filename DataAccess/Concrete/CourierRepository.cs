11d132dfb72817e8469eddf080271980a1318a75
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-19 17:11:47 +0300

    Concrete of DataAccess created, Constructor added

---
+++ DataAccess/Concrete/CourierRepository.cs
@@ -0,0 +1,19 @@
+﻿using Core.DataAccess.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.Context;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class CourierRepository : EntityRepositoryBase<Courier, BaseDbContext>, ICourierRepository
+    {
+        public CourierRepository(BaseDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ DataAccess/Concrete/PharmacyRepository.cs
@@ -0,0 +1,19 @@
+﻿using Core.DataAccess.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.Context;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class PharmacyRepository : EntityRepositoryBase<Pharmacy, BaseDbContext>, IPharmacyRepository
+    {
+        public PharmacyRepository(BaseDbContext context) : base(context)
+        {
+        }
+    }
+}
