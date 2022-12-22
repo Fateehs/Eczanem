a2c164e5afcce3b7d2b14fe1ea269433a4cce37a
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-22 21:46:24 +0300

    Manager modified, Select and CRUD Operations added

--- Business/Concrete/MedicineManager.cs
+++ Business/Concrete/MedicineManager.cs
@@ -1,4 +1,9 @@
 ﻿using Business.Abstract;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +14,45 @@ namespace Business.Concrete
 {
     public class MedicineManager : IMedicineService
     {
+        IMedicineRepository _medicineRepository;
+
+        public MedicineManager(IMedicineRepository medicineRepository)
+        {
+            _medicineRepository = medicineRepository;
+        }
+        public IDataResult<List<Medicine>> GetAll()
+        {
+            return new SuccessDataResult<List<Medicine>>(_medicineRepository.GetAll());
+        }
+
+        public IDataResult<Medicine> GetById(int id)
+        {
+            return new SuccessDataResult<Medicine>(_medicineRepository.Get(p => p.Id == id));
+        }
+
+        public IResult Add(Medicine medicine)
+        {
+            _medicineRepository.Add(medicine);
+
+            return new SuccessResult();
+        }
+
+        public IResult Update(Medicine medicine)
+        {
+            _medicineRepository.Update(medicine);
+
+            return new SuccessResult();
+        }
+
+        public IResult Delete(int id)
+        {
+            var medicine = _medicineRepository.Get(m => m.Id == id);
+
+            if (medicine != null)
+
+                _medicineRepository.Delete(medicine);
+
+            return new SuccessResult();
+        }
     }
 }
