7dc55b4630b23cc1fb3a98731798ac05e81317e1
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-22 21:48:11 +0300

    Controller modified, CRUD and Select Operations added

--- WebAPI/Controllers/MedicinesController.cs
+++ WebAPI/Controllers/MedicinesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
@@ -7,5 +9,56 @@ namespace WebAPI.Controllers
     [ApiController]
     public class MedicinesController : ControllerBase
     {
+        IMedicineService _medicineService;
+
+        public MedicinesController(IMedicineService medicineService)
+        {
+            _medicineService = medicineService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _medicineService.GetAll();
+
+            if (!result.Success) return BadRequest(result);
+            return Ok(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _medicineService.GetById(id);
+
+            if (!result.Success) return BadRequest(result);
+            return Ok(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Medicine medicine)
+        {
+            var result = _medicineService.Add(medicine);
+
+            if (!result.Success) return BadRequest(result);
+            return Ok(result);
+        }
+
+        [HttpPost("update")]
+        public IActionResult Update(Medicine medicine)
+        {
+            var result = _medicineService.Update(medicine);
+
+            if (!result.Success) return BadRequest(result);
+            return Ok(result);
+        }
+
+        [HttpPost("delete")]
+        public IActionResult Delete(int id)
+        {
+            var result = _medicineService.Delete(id);
+
+            if (!result.Success) return BadRequest(result);
+            return Ok(result);
+        }
     }
 }
