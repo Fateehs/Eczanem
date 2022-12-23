e0c06422f9bd7792d20ee5a86e97c031d80e25a3
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-23 18:12:05 +0300

    Service modified, Method added

--- Business/Abstract/IPharmacyService.cs
+++ Business/Abstract/IPharmacyService.cs
@@ -12,6 +12,7 @@ namespace Business.Abstract
     {
         IDataResult<List<Pharmacy>> GetAll();
         IDataResult<Pharmacy> GetById(int id);
+        IResult ReadyForDelivery(Order order);
         IResult Add(Pharmacy pharmacy);
         IResult Update(Pharmacy pharmacy);
         IResult Delete(int id);
