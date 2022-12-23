b09eb412c1aface576b3a4f23ccb08d3b067e77d
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-23 18:18:32 +0300

    Entity created, Properties added

---
+++ Entities/Concrete/Order.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Order : IEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int MedicineId { get; set; }
+        public int? CourierId { get; set; }
+        public bool ReadyForDelivery { get; set; } = false;
+        public string OrderNumber { get; set; }
+    }
+}
