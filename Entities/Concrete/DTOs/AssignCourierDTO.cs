924cd5acabcf7870e27fa0b518f4fece42fbe94b
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-23 18:17:33 +0300

    DTO created, Properties added

---
+++ Entities/Concrete/DTOs/AssignCourierDTO.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete.DTOs
+{
+    public class AssignCourierDTO : IDTO
+    {
+        public int OrderId { get; set; }
+        public int UserId { get; set; }
+        public int CourierId { get; set; }
+        public int MedicineId { get; set; }
+        public string OrderNumber { get; set; }
+        public bool ReadyForDelivery { get; set; }
+    }
+}
