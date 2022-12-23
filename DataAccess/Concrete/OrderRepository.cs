cc9a2ff25e58acf9b506c18f10c089ff42d6beeb
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-23 18:15:42 +0300

    Concrete of Data Access created

---
+++ DataAccess/Concrete/OrderRepository.cs
@@ -0,0 +1,19 @@
+﻿using Core.DataAccess.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.Context;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class OrderRepository : EntityRepositoryBase<Order, BaseDbContext>, IOrderRepository
+    {
+        public OrderRepository(BaseDbContext context) : base(context)
+        {
+        }
+    }
+}
