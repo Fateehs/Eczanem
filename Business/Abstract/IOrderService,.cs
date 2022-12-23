83aab622ba17754912020c271143381a8e7215ab
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-23 18:11:45 +0300

    Service created, Select and CRUD Operations added

---
+++ Business/Abstract/IOrderService,.cs
@@ -0,0 +1,20 @@
+﻿using Core.Utilities.Results.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IOrderService
+    {
+        IDataResult<List<Order>> GetAll();
+        IDataResult<Order> GetById(int id);
+        IDataResult<Order> GetDetailsByNumber(string number);
+        IResult Add(Order order);
+        IResult Update(Order order);
+        IResult Delete(int id);
+    }
+}
