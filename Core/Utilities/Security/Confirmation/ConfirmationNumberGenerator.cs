8880d5c7dd17d037259ac2c9431020671d51000e
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-28 18:50:32 +0300

    Class created, Method added

---
+++ Core/Utilities/Security/Confirmation/ConfirmationNumberGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.Confirmation
+{
+    public class ConfirmationNumberGenerator
+    {
+        public static void Generate()
+        {
+            string timestamp = DateTime.Now.ToString("yyyyMMddHHmmssffff");
+
+            byte[] randomNumber = new byte[4];
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            rng.GetBytes(randomNumber);
+
+            string confirmationNumber = timestamp + BitConverter.ToUInt32(randomNumber, 0).ToString();
+        }
+    }
+}
