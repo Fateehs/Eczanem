6827ee64c95e912e287a4ab07ea88a3713018d37
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-29 19:38:56 +0300

    DTO created, Properties added

---
+++ Entities/Concrete/DTOs/CustomerOrderDTO.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete.DTOs
+{
+    public class CustomerOrderDTO : IDTO
+    {
+        public int OrderId { get; set; }
+        public int PharmacyId { get; set; }
+        public int CustomerId { get; set; }
+        public int MedicineId { get; set; }
+        public int? CourierId { get; set; }
+        public bool OrderAcceptedFromPharmacy { get; set; } = false;
+        public bool ReadyForDelivery { get; set; } = false;
+        public string OrderNumber { get; set; }
+    }
+}
