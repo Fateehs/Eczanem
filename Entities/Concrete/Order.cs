52f5cb3a6a0913348163b1d34f093dc88dfea169
Fatih Enes Selvi <benzer1010@gmail.com>
2022-12-30 16:56:23 +0300

    Entity modified, Properties added

--- Entities/Concrete/Order.cs
+++ Entities/Concrete/Order.cs
@@ -14,8 +14,13 @@ namespace Entities.Concrete
         public int CustomerId { get; set; }
         public int MedicineId { get; set; }
         public int? CourierId { get; set; }
+        public bool OrderDelivered { get; set; } = false;
+        public bool CourierOnTheWay { get; set; } = false;
+        public bool GetPackageFromPharmacy { get; set; } = false;
+        public bool GivePackageToCourier { get; set; } = false;
         public bool OrderAcceptedFromPharmacy { get; set; } = false;
         public bool ReadyForDelivery { get; set; } = false;
+        public string ConfirmationNumberFromCustomer { get; set; }
         public string OrderNumber { get; set; }
     }
 }
